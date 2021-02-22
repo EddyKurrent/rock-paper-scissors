ed2a61cd533f099740dfd0b57fe9d85869d0ed58
Dee Darby <ddarby@frostyacres.com>
2021-02-22 01:28:04 -0500

    [DD] Fake changes 1:
     * Edit words.

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class Game {
 	
 	public static void main(String[] args) {
-		String prompt = "Will it be rock, paper or scissors?";
+		String prompt = "Will it be rock, paper or scissorses?";
 		String input = JOptionPane.showInputDialog(prompt);
 		System.out.println("You selected: " + input);
 	}
